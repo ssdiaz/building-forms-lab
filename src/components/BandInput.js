493386ad8d3bb44819bff1b3079cee842dcc94e1
Samantha Diaz <samidiaz3@gmail.com>
2022-03-16 16:53:57 -0400

    commit tree 312eed7d8f9bdf04d8d58a391f91b2d2737220a6

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,12 +2,46 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  state = {
+    name: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({
+      name: '',
+    });
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="name"
+            onChange={this.handleChange} 
+            value = {this.state.name} />
+          <input type="submit" />
+        </form>
+        {this.state.name}
       </div>
     )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: (formData) => dispatch({type: "ADD_BAND", payload: formData })
   }
 }
 
